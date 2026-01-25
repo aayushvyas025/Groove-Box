7d78bb8e9dac9f41d42c4767507a37197d2a86f7
Aayush Vyas <aayush25vyas@gmail.com>
2026-01-25 22:37:29 +0530

    Using common middleware in server file

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,20 @@
 import express from "express";
 import { connectionWithDB } from "./src/helper/utils/connectionWithDB.js";
+import { middlewareRoutes, parseJson } from "./src/middleware/common/common.middleware.js";
 
+const {userRoute, albumRoute, songRoute, adminRoute, authRoute, statsRoute} = middlewareRoutes; 
 
 
 const app = express();
+
+parseJson(app);
+userRoute(app); 
+albumRoute(app); 
+songRoute(app);
+adminRoute(app);
+authRoute(app);
+statsRoute(app); 
+
+
 
 connectionWithDB(app);
