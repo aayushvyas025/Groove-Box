1539bd7618781d3645b0821fc167502834232fa4
Aayush Vyas <aayush25vyas@gmail.com>
2026-02-04 19:08:48 +0530

    stats controller fetcAllStatistics logic implemented

--- backend/src/controller/stats/stats.controller.js
+++ backend/src/controller/stats/stats.controller.js
@@ -3,17 +3,40 @@ import { Album } from "../../model/album/album.model.js";
 import { Song } from "../../model/song/song.model.js";
 import { User } from "../../model/user/user.model.js";
 
-const {apiResponses, statusCode, statsMessages} = serverMessages;
-const { fetchAllStats, fetchAllStatsError} = statsMessages;
+const { apiResponses, statusCode, statsMessages } = serverMessages;
+const { fetchAllStats, fetchAllStatsError } = statsMessages;
 
-export const fetchAllStatistics = async (request, response, next) => {
+export const fetchAllStatistics = async (_, response, next) => {
   try {
-    const stats = await Promise.all({
-      totalSongs: await Song.countDocuments(),
-      totalAlbums: await Album.countDocuments(),
-      totalUsers: await User.countDocuments(),
+    const [totalSongs, totalAlbums, totalUsers, allArtists] = await Promise.all(
+      [
+        Song.countDocuments(),
+        Album.countDocuments(),
+        User.countDocuments(),
+        Song.aggregate([
+          { $project: { artist: 1 } },
+          {
+            $unionWith: {
+              coll: "albums",
+              pipeline: [{ $project: { artist: 1 } }],
+            },
+          },
+          { $match: { artist: { $ne: null } } },
+          { $group: { _id: "$artist" } },
+          { $count: "count" },
+        ]),
+      ],
+    );
+    response.status(statusCode.ok).json({
+      success: apiResponses.success,
+      message: fetchAllStats,
+      stats: {
+        totalSongs,
+        totalAlbums,
+        totalUsers,
+        totalArtists: allArtists[0]?.count || 0,
+      },
     });
-    response.status(statusCode.ok).json({success:apiResponses.success, message:fetchAllStats, stats});
   } catch (error) {
     console.error(`${fetchAllStatsError} : ${error.message}`);
     next(error);
